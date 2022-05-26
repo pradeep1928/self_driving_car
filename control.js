c9241a3e87ca406eb6d8444852af95e786a6efd8
Pradeep Padmukhi <ppadmukhi28@gmail.com>
2022-05-26 21:41:11 +0530

    added traffic demo car simulation

--- control.js
+++ control.js
@@ -1,11 +1,20 @@
 class Controls {
-    constructor() {
+    constructor(type) {
         this.forward = false,
             this.left = false,
             this.right = false,
             this.reverse = false
 
-        this.#addKeyBoardListeners();
+        switch (type) {
+            case "KEYS":
+                this.#addKeyBoardListeners();
+                break;
+            case 'DUMMY':
+                this.forward = true;
+                break;
+
+        }
+
     }
 
     #addKeyBoardListeners() {
